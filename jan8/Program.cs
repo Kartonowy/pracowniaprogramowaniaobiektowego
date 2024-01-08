4efeaab02f659bbd5ea784c9acb1c6a8cedcc51c
Kartonowy <ttrollface.com.pl@gmail.com>
2024-01-08 12:46:03 +0100

    January 8th 1st

---
+++ jan8/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace jan8;
+class Program
+{
+    /*
+ 1) Napisz program, który wczytuje z klawiatury liczbę n, a następnie n imion i wieków
+osób.
+2) Użyj tablicy, aby przechować wczytane imiona i wieki.
+3) Użyj listy, aby przechować tylko te imiona, które zaczynają się na literę A.
+4) Użyj słownika, aby przechować pary (imie, wiek) dla wszystkich osób, których wiek jest
+większy niż 18 lat.
+5) Wypisz na ekranie zawartość tablicy, listy i słownika.
+*/
+
+    static void Main(string[] args)
+
+    {
+        int n = ReadInt("Podaj liczbę osób: ", 0, int.MaxValue);
+        Console.WriteLine("Hello, World!");
+    }
+    static int ReadInt(string prompt, int low, int high) {
+        int result;
+        bool valid;
+        do {
+            Console.Write(prompt);
+            valid = int.TryParse(Console.ReadLine(), out result) && result >= low && result <= high;
+            if (!valid) {
+                Console.WriteLine($"Podaj liczbe z zakresu: {low} {high}");
+            }
+        } while (!valid);
+        return result;
+
+    }
+}
